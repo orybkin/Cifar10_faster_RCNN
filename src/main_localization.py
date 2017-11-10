4e90ab907e06d475e40feac906c92a68f6648cf1
Oleh Rybkin <oleh.rybkin@gmail.com>
2017-11-09 22:03:10 -0500

    Third task

--- src/main_localization.py
+++ src/main_localization.py
@@ -7,7 +7,7 @@ from data_loader_localization import get_loader
 from utils import prepare_dirs_and_logger, save_config
 from models import *
 
-def main(config, model):
+def main(config, model, conv_block):
   prepare_dirs_and_logger(config)
 
   rng = np.random.RandomState(config.random_seed)
@@ -25,7 +25,7 @@ def main(config, model):
 
   trainer = Trainer(config, train_data_loader, train_label_loader, test_data_loader, test_label_loader,
                     train_location_loader, train_size_loader,  test_location_loader, test_size_loader,
-                    train_msk_loader, test_msk_loader, model)
+                    train_msk_loader, test_msk_loader, model, conv_block)
   if config.is_train:
     save_config(config)
     trainer.train()
@@ -37,9 +37,14 @@ def main(config, model):
 if __name__ == "__main__":
   config, unparsed = get_config()
 
-  # Task 2
+  # Task 3
 
-  # Subtask 1
+  # model=RCNN
+  # main(config, model, conv_factory)
+
+  # model=RCNN
+  # main(config, model, sepconv_factory)
+
   model=RCNN
-  main(config, model)
+  main(config, model, resblock_factory)
 
